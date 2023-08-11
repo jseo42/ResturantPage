4049c93d8035d1fc25041bd443696ec54614d99a
jseo42 <jayseo.js@gmail.com>
2023-08-10 22:32:57 -0500

    fixed menu button function

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,5 +1,6 @@
 
 function createMenu() {
+  
   const menu = document.createElement('div');
   menu.classList.add('menu');
 
@@ -9,17 +10,14 @@ function createMenu() {
   const menuPanel = document.createElement('div');
   menuPanel.setAttribute('id','menu-title');
   
-
-
   menu.appendChild(menuTitle);
   menu.appendChild(menuPanel);
+  menuPanel.appendChild(createPanel());
   
-
   return menu;
   
 };
-
-function createMenuArray(){
+function createPanel() {
   const menuList = [
     {name: "Food Item", price: '$xx.xx', desc: 'Lorem'},
     {name: "Food Item", price: '$xx.xx', desc: 'Lorem'},
@@ -29,15 +27,35 @@ function createMenuArray(){
     {name: "Food Item", price: '$xx.xx', desc: 'Lorem'}
   ]
 
-  const menuArray = menuList.map(obj => [obj.name, obj.price, obj.desc]);
-  return menuArray 
+  const dishList = menuList.map(obj => [obj.name, obj.price, obj.desc]);
+  let menuItems = document.createElement('div');
 
-};
 
-function addElements(menuArray){
-  
+  dishList.forEach(dish => {
+    let dishElement = document.createElement('div');
+    dishElement.classList.add('dish-element');
+
+    let dishTitle = document.createElement('p');
+    dishTitle.classList.add('dish-title');
+    dishTitle.textContent = dish[0];
+    dishElement.appendChild(dishTitle)
+
+    let dishPrice = document.createElement('p');
+    dishPrice.classList.add('dish-price');
+    dishPrice.textContent = dish[1];
+    dishElement.appendChild(dishPrice);
+
+    let dishDesc = document.createElement('p');
+    dishDesc.classList.add('dish-desc');
+    dishDesc.textContent = dish[2];  
+    dishElement.appendChild(dishDesc)
+
+    menuItems.appendChild(dishElement);
+  });
+
+  return menuItems;
+
 }
-
 
 
 function loadMenu() {
